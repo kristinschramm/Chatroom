7a10e004a2ab704d747e9b94712bb7648419a45b
Rakesh <rbaruah@gmail.com>
2018-05-21 11:30:45 -0500

    test commit

--- Client/UI.cs
+++ Client/UI.cs
@@ -14,7 +14,7 @@ namespace Client
         }
         public static string GetInput()
         {
-            return Console.ReadLine();
+            return Console.ReadLine(); 
         }
     }
 }
