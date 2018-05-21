eaa43e48677cd545a9244af5febf8f350d2d7b64
kristinschramm <kristin.schramm@gmail.com>
2018-05-21 12:04:25 -0500

    Added Text Logger and chatroom classes

--- Server/TextLogger.cs
+++ Server/TextLogger.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Server
 {
-    class TextLogger: ILogger
+    class TextLogger : ILogger
     {
+
+        public string filePath = @"C:\Chatroom\Server\ChatLog.txt"; //create text logger with chatroom change name with room name
+        public void Log(string message)
+        {
+            if (!File.Exists(filePath))
+            {
+                using (StreamWriter streamWriter = File.CreateText(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter streamWriter = File.AppendText(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                }
+
+            }
+
+            }
+        }
     }
 }
